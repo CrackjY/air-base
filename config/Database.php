8730cd4e0223ca143e665ba01d56e380ea9308a7
yassine <yassine.latti@econocom.com>
2019-01-12 17:48:16 +0100

    fix parameter

---
+++ config/Database.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Config;
+
+/**
+ * Class Database
+ * @package Config
+ */
+class Database extends Parameter
+{
+    /**
+     * @var $db
+     */
+    private $db;
+
+    public function dbConnect()
+    {
+        return $this->db = new \PDO("mysql:host=".$this->getHost().";dbname=".$this->getName().";", $this->getUser(), $this->getPassword());
+    }
+}
---
+++ config/Routing.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Config;
+
+use Helper\Instance\Instance;
+
+class Routing extends Instance
+{
+    public function __construct()
+    {
+        parent::__construct();
+
+        if ($_SERVER['REQUEST_URI'] == '/') {
+            $this->frontController->homeAction();
+        }
+
+        if (isset($_GET['page'])) {
+            switch ($_GET['page']) {
+                //Front
+
+                //Back
+                case 'admin';
+                    $this->backController->homeAction();
+                    exit;
+                //City
+                case 'admin/cities';
+                    $this->cityController->listAction();
+                    exit;
+                case 'admin/cities/new';
+                    $this->cityController->newAction();
+                    exit;
+                //Airplane
+                case 'admin/airplanes';
+                    $this->airplaneController->listAction();
+                    exit;
+                case 'admin/airplanes/new';
+                    $this->airplaneController->newAction();
+                    exit;
+                //Type
+                case 'admin/types';
+                    $this->typeController->listAction();
+                    exit;
+                case 'admin/types/new';
+                    $this->typeController->newAction();
+                    exit;
+                //Pilot
+                case 'admin/pilots';
+                    $this->pilotController->listAction();
+                    exit;
+                case 'admin/pilots/new';
+                    $this->pilotController->newAction();
+                    exit;
+                //Flight
+                case 'admin/flights';
+                    $this->flightController->listAction();
+                    exit;
+                case 'admin/flights/new';
+                    $this->flightController->newAction();
+                    exit;
+                case 'admin/flights/edit';
+                    $this->flightController->editAction();
+                    exit;
+                case 'admin/flights/delete';
+                    $this->flightController->deleteAction();
+                    exit;
+
+                //API
+                case 'api/admin/flights';
+                    $this->flightController->flightDataJson();
+                    exit;
+            }
+        }
+    }
+}
